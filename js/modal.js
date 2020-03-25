700ae89c983cfd4639ae04bfd817ba88ad1c63c1
Hubert Łepski <hubertlepsky@gmail.com>
2020-03-25 19:28:57 +0100

    improve text on homepage

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,6 @@
 'use strict';
 
 (function() {
-    //Teraz wystarczy napisać funkcję otwierającą modal:
 
     var modalLinks = document.querySelectorAll('.show-modal');
     var modals = document.querySelectorAll('.modal');
@@ -14,23 +13,15 @@
         };
 
         var attribute = this.getAttribute('href');
-        console.log(attribute); // do usuniecia
         var targetElement = document.querySelector(attribute); //modalHref
         targetElement.classList.add('show'); //modalHref
         document.querySelector('#modal-overlay').classList.add('show');
 
     };
 
-
-    // Mimo, że obecnie mamy tylko jeden link, stosujemy kod dla wielu linków. W ten sposób nie będzie trzeba go zmieniać, kiedy zechcemy mieć więcej linków lub guzików otwierających modale
-
-
     for (var i = 0; i < modalLinks.length; i++) {
         modalLinks[i].addEventListener('click', showModal);
     }
-
-
-    // Dodajemy też funkcję zamykającą modal, oraz przywiązujemy ją do kliknięć na elemencie z klasą "close".
 
     var hideModal = function(event) {
         event.preventDefault();
@@ -43,21 +34,12 @@
         closeButtons[i].addEventListener('click', hideModal);
     }
 
-
-    // Dobrą praktyką jest również umożliwianie zamykania modala poprzez kliknięcie w overlay.
-
     document.querySelector('#modal-overlay').addEventListener('click', hideModal);
-
-
-    // Musimy jednak pamiętać, aby zablokować propagację kliknięć z samego modala - inaczej każde kliknięcie wewnątrz modala również zamykałoby go.
-
 
     for (var i = 0; i < modals.length; i++) {
         modals[i].addEventListener('click', function(event) {
             event.stopPropagation();
         });
     }
-
-    // I to wszystko - mamy już działający modal!
 
 })();
